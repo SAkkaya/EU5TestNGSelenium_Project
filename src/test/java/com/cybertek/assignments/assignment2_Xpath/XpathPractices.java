2b02cd7f0c693a45830c86b512b160a022f82697
SAkkaya <sadikakkaya.sa@gmail.com>
2021-06-08 21:46:12 +0200

    8 June 2021 - 3st push

--- src/test/java/com/cybertek/assignments/assignment2_Xpath/XpathPractices.java
+++ src/test/java/com/cybertek/assignments/assignment2_Xpath/XpathPractices.java
@@ -15,6 +15,8 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class XpathPractices {
 
@@ -39,24 +41,42 @@ public class XpathPractices {
         WebElement emailInputBox = driver.findElement(By.xpath("//input[@name='email']"));
 
 //        e. “Retrieve password” button
+        WebElement retrieveButton=driver.findElement(By.xpath("//button[@id='form_submit']"));
 
 
-
-
-//        4.Print text of a,b,c,e and put some email to d
-
+//        4.Print text of a,b,c,e
+        //Print text of a
         String homeLinkText = homeLink.getText();
         System.out.println("homeLinkText = " + homeLinkText);
-
+        //Print text of b
         String forgetPasswordHeaderText = forgetPasswordHeader.getText();
         System.out.println("forgetPasswordHeaderText = " + forgetPasswordHeaderText);
-
+        //Print text of c
         String emailText=email.getText();
         System.out.println("emailText = " + emailText);
 
+        //put some email to d
         emailInputBox.sendKeys("kaya@gmail.com");
         String enteredEmail = emailInputBox.getAttribute("value");
         System.out.println("enteredEmail = " + enteredEmail);
+
+        //Print text of e
+        String retrieveButtonText=retrieveButton.getText();
+        System.out.println("retrieveButtonText = " + retrieveButtonText);
+        retrieveButton.click();
+        String confirmationMessage=driver.findElement(By.xpath("//h4[@name='confirmation_message']")).getText();
+
+
+            if(confirmationMessage.equals("Your e-mail's been sent!")){
+                System.out.println("PASS");
+                System.out.println("confirmation Message = " + confirmationMessage);
+            }else{
+                System.out.println("FAIL");
+                System.out.println("confirmation Message = " + confirmationMessage);
+                System.out.println("expected Message = " + "Your e-mail's been sent!");
+            }
+
+
 
 
 
