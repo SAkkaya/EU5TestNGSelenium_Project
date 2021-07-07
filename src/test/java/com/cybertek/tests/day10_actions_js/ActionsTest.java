d94464125e8a1fe2058843a9a886a9589418fae1
SAkkaya <sadikakkaya.sa@gmail.com>
2021-07-08 00:48:55 +0200

    EU_5

--- src/test/java/com/cybertek/tests/day10_actions_js/ActionsTest.java
+++ src/test/java/com/cybertek/tests/day10_actions_js/ActionsTest.java
@@ -1,22 +1,25 @@
 package com.cybertek.tests.day10_actions_js;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class ActionsTest {
 
-    // WebDriverWait wait;
     WebDriver driver;
 
 
     @BeforeMethod
     public void setUp() {
         driver = WebDriverFactory.getDriver("chrome");
-        // wait = new WebDriverWait(driver, 10); // creating wait object from  WebDriverWait class
 
     }
 
@@ -28,9 +31,50 @@ public class ActionsTest {
     }
 
     @Test
-    public void hoverTest(){
+    public void hoverTest() throws InterruptedException {
+
+    driver.get("http://practice.cybertekschool.com/hovers");
+
+        WebElement img1 = driver.findElement(By.tagName("img"));
+
+        //Actions --> class that contains all the user interactions
+        //how to create
+        Actions actions = new Actions(driver);
+
+        //perform () --> perform the action, complete the action
+        //moveToElement --> move your mouse to webelement(hover over)
+        Thread.sleep(2000);
+        actions.moveToElement(img1).perform();
+
+        WebElement viewLink = driver.findElement(By.linkText("View profile"));
+        Assert.assertTrue(viewLink.isDisplayed(),"verify view Link is displayed");
+
+    }
+
+    @Test
+    public void dragAndDrop() throws InterruptedException {
+
+        driver.get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+
+        // findElements will not throw NoSuch element exception, if it cannot find element, only return an empty List so we use List and findlements
+        List<WebElement> acceptAndClose = driver.findElements(By.xpath("//*[.='Accept and Close']"));
+        List<WebElement> acceptCookies= driver.findElements(By.xpath("//*[.='Accept Cookies']"));
+        Thread.sleep(1000);
+        if (acceptAndClose.size()>0){
+            acceptAndClose.get(0).click();
+        }else if (acceptCookies.size()>0){
+            acceptCookies.get(0).click();
+        }
 
 
+        WebElement source = driver.findElement(By.id("draggable"));
+        WebElement target = driver.findElement(By.id("droptarget"));
+
+        Thread.sleep(2000);
+        actions.dragAndDrop(source, target).perform();
 
 
     }
