1f3d10e48fab30c5d3d9c974e84b7820ea805f3c
SAkkaya <sadikakkaya.sa@gmail.com>
2021-07-26 14:55:40 +0200

    EU_5

--- src/test/java/com/cybertek/tests/day14_extent_reports/ExtentDemoTest.java
+++ src/test/java/com/cybertek/tests/day14_extent_reports/ExtentDemoTest.java
@@ -3,7 +3,10 @@ package com.cybertek.tests.day14_extent_reports;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.cybertek.utilities.ConfigurationReader;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class ExtentDemoTest {
 
@@ -15,7 +18,7 @@ public class ExtentDemoTest {
     ExtentTest extentLogger;
 
     @BeforeMethod  // we are not extendingBaseClass here , just separate class
-    public void setUp(){
+    public void setUp(){  // this part is static for copy pasting for all test cases
          //initialize the class
         report = new ExtentReports();
 
@@ -32,7 +35,39 @@ public class ExtentDemoTest {
         //title in report
         htmlReporter.config().setReportName("Vytrack Smoke Test");
 
-        //
+        //set environment information
+        report.setSystemInfo("Environment", "QA");
+        report.setSystemInfo("Browser", ConfigurationReader.get("browser"));
+        report.setSystemInfo("OS",System.getProperty("os.name"));
+
+
+    }
+
+    @Test
+    public void test1(){  // here is the dynamic part
+        //give name to current test
+        extentLogger= report.createTest("TC123 Login as Driver Test");
+
+        //test steps
+        extentLogger.info("Open Chrome Browser");
+
+        extentLogger.info("Go to this URL");
+
+        extentLogger.info("Enter driver username password");
+
+        extentLogger.info("Click Login");
+
+        extentLogger.info("Verify logged in");
+
+        //pass() --> marks the test case as passed
+        extentLogger.pass("TC123 is passed");
+
+    }
+
+    @AfterMethod
+    public void teardown(){
+        //this is when the report is actually created
+        report.flush();
 
     }
 
